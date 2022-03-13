1cae21391441a41ae3df9fdadff937706be22471
MichalMusial <michalmusial553@gmail.com>
2022-03-13 12:31:50 +0100

    docker :)

--- README.md
+++
@@ -1,5 +0,0 @@
-# help-ukraine
-# jak uruchomić?
-# mvn clean package
-# java -jar help-ukraine-app/target/help-ukraine-app-0.0.0-SNAPSHOT.jar
-# open localhost:8080
--- help-ukraine-frontend/src/App.js
+++ help-ukraine-frontend/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState }  from 'react';
+import axios from 'axios';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'html-react-parser';
+
 
 function App() {
+  const [message, setMessage] = useState("LOADING_THE_MESSAGE...");
+  React.useEffect(effect => {
+    axios.get(`/hello`)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+      setMessage(res.data);
+    })
+  }, []);
+
 
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          Wiadomość z backendu: [<code><div> { ReactHtmlParser (message) } </div></code>].
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Superanckoooo
         </a>
       </header>
     </div>
