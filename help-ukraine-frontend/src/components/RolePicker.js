97719f85baebe9e7412d5282bac8e4acb4602a60
MichalMusial <michalmusial553@gmail.com>
2022-03-27 10:26:38 +0200

    ekran poczatkowy

--- help-ukraine-frontend/src/components/RolePicker.js
+++ help-ukraine-frontend/src/components/RolePicker.js
@@ -1,9 +1,44 @@
-import React  from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import Card from './Card'
 
+const RolePickerBody = styled.div`
+    height: 100vh;
+    background-color: var(--ukrainski-niebieski);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;`;
 
-const RolePicker = () =>  {
+const RolePickerButton = styled(Card)`
+    height: 30vh;
+    background: white;
+    width: 30vh;
+    color: var(--ukrainski-niebieski);
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size:24px;
+    font-weight: 600;
+    }`;
+const RolePickerButtonDistancer = styled.div`
+    background-color: var(--ukrainski-niebieski);
+    height: 5vh;`;
+
+const RolePicker = () => {
     return (
-        <div className="role-picker"></div>
+        <RolePickerBody>
+            <RolePickerButton className="card card-1">
+                I need help
+            </RolePickerButton>
+            <RolePickerButtonDistancer>
+            </RolePickerButtonDistancer>
+            <RolePickerButton className="card-1">
+                I can help
+            </RolePickerButton>
+        </RolePickerBody>
     );
 }
 export default RolePicker;
