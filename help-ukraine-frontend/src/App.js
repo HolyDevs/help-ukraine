a9008bc612a7df2db0b79388ba3c94667a3f2063
MichalMusial <michalmusial553@gmail.com>
2022-03-27 13:10:26 +0200

    ekran logowania setup

--- help-ukraine-frontend/src/App.js
+++ help-ukraine-frontend/src/App.js
@@ -4,17 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RolePicker from "./screens/RolePicker";
 import Refugee from "./screens/Refugee";
 import Volunteer from "./screens/Volunteer";
+import Login from "./screens/Login";
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/" element={<RolePicker />}>
+              <Route path="/" element={<Login />}>
+              </Route>
+              <Route path="/rolePicker" element={<RolePicker />}>
               </Route>
               <Route path="/volunteer" element={<Volunteer />}>
               </Route>
               <Route path="/refugee" element={<Refugee />}>
               </Route>
+
           </Routes>
       </BrowserRouter>
   );
