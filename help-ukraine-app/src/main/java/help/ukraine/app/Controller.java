099f075d422ce9182e8289ee190ff1a5ff4ff03c
ninjarlz <michal.kusmidrowicz@gmail.com>
2022-03-28 12:24:18 +0200

    backend config 1

--- help-ukraine-app/src/main/java/help/ukraine/app/Controller.java
+++ help-ukraine-app/src/main/java/help/ukraine/app/Controller.java
@@ -1,13 +1,16 @@
 package help.ukraine.app;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class Controller {
 
    @GetMapping("hello")
    public String get() {
+      log.debug("hello endpoint hit");
       return "<h2>Hello Ukraine Kapitanie</h2>";
    }
 }
