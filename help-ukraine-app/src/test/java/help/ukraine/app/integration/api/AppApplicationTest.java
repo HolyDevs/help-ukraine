4a1fbdcab1a603eeec95667ea2e7f928552b6267
ninjarlz <michal.kusmidrowicz@gmail.com>
2022-03-30 18:00:28 +0200

    unit tests config ready

--- help-ukraine-app/src/test/java/help/ukraine/app/integration/api/AppApplicationTest.java
+++ help-ukraine-app/src/test/java/help/ukraine/app/integration/api/AppApplicationTest.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import help.ukraine.app.data.UserEntity;
 import help.ukraine.app.model.UserModel;
 import help.ukraine.app.repository.UserRepository;
-import ma.glasnost.orika.MapperFacade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,8 +20,9 @@ import javax.transaction.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = "integrationtest")
 class AppApplicationTest {
--- help-ukraine-app/src/test/java/help/ukraine/app/unit/service/UserServiceTest.java
+++ help-ukraine-app/src/test/java/help/ukraine/app/unit/service/UserServiceTest.java
@@ -1,4 +1,54 @@
 package help.ukraine.app.unit.service;
 
-public class UserServiceTest {
+import help.ukraine.app.data.UserEntity;
+import help.ukraine.app.exception.DataNotExistsException;
+import help.ukraine.app.model.UserModel;
+import help.ukraine.app.repository.UserRepository;
+import help.ukraine.app.service.UserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles(profiles = "unittest")
+class UserServiceTest {
+    private static final String EXISTING_USER_ID = "666";
+    private static final String NOT_EXISTING_USER_ID = "667";
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    void setUpMock() {
+        UserEntity userEntity = new UserEntity(EXISTING_USER_ID, "Jan", "Testowy", "aaa");
+        doReturn(Optional.of(userEntity)).when(userRepository).findById(EXISTING_USER_ID);
+        doReturn(Optional.empty()).when(userRepository).findById(NOT_EXISTING_USER_ID);
+    }
+
+    @Test
+    void fetchUserOkTest() throws Exception {
+        UserModel userModel = userService.getUser(EXISTING_USER_ID);
+        assertEquals(EXISTING_USER_ID, userModel.getId());
+    }
+
+    @Test
+    void fetchUserNotFoundTest() {
+        assertThrows(DataNotExistsException.class, () -> userService.getUser(NOT_EXISTING_USER_ID));
+    }
 }
